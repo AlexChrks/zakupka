b16bf56c972f4daa58bf03810b430c60561b8ef8
Alexey Cherkas <alexey.cherkas1997@gmail.com>
2026-01-30 15:44:53 +0100

    remove build

--- .next/dev/static/chunks/app_(dashboard)_my-rfqs_page_tsx_888abbf4._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_d4dacaa6._.js",
-  "static/chunks/node_modules_9dfe1f01._.js"
-],
-    source: "dynamic"
-});
--- .next/server/chunks/ssr/app_(auth)_layout_tsx_2ffca1fc._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[12498,a=>{"use strict";var b=a.i(7997);function c({children:a}){return(0,b.jsx)("div",{className:"flex min-h-screen flex-col items-center justify-center bg-muted/40 p-4",children:a})}a.s(["default",()=>c])}];
-
-//# sourceMappingURL=app_%28auth%29_layout_tsx_2ffca1fc._.js.map
